48fb17da5b4dbdfac954804e76ec118fd6f28c17
olfe <olfe@olfe.(none)>
2013-02-28 20:57:40 +0100

    Minor rendering adjustements to search result display

--- tschercha/views.py
+++ tschercha/views.py
@@ -27,20 +27,19 @@ def search(data):
     sql = "SELECT m, n FROM dicziunari WHERE m LIKE '%%%s%%' OR n LIKE '%%%s%%'" % (query, query)
     cursor.execute(sql)
     res = cursor.fetchall()
-    
-    if len(res) == 0:
-        res = [("", "Ingüns resultats.")]
+
     return res
     
 @login_required
 def tschercha(request):
-    result = [("", "Tü (eu poss schon dir tü, o?) nun hast amo tscherchà alch.")]
-    if request.method == 'POST': # If the form has been submitted...
-        form = SearchForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    result = []
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
             result = search(form.cleaned_data)
     else:
         form = SearchForm()
 
-    return render(request, 'tschercha.html', {'form': form, 'result':result,})
+    return render(request, 'tschercha.html',
+                  {'form': form, 'result':result,})
 
