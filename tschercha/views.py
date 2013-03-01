a9300ac397f4c8b2c7fe2a24bd5dbe5b2d17f953
Uli Franke <cls@nebadje.org>
2013-03-01 01:17:19 +0100

    Handle idioms in a better way (in case we should add others)

--- tschercha/views.py
+++ tschercha/views.py
@@ -3,27 +3,44 @@
 
 # http://devdoodles.wordpress.com/2009/02/16/user-authentication-with-django-registration/
 # http://docs.b-list.org/django-registration/0.8/quickstart.html
-# http://jagdeepmalhi.blogspot.ch/2010/09/django-application-contact-form.html
 
 import sqlite3, os
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django import forms
 
+IDIOM_VALLADER = '0'
+IDIOM_PUTER    = '1'
+IDIOM_DEFAULT  = IDIOM_VALLADER
+
+IDIOM_NAMES = {IDIOM_VALLADER : 'Vallader',
+               IDIOM_PUTER    : 'Puter',
+               }
+
+def idiomNameToIdiom(idiomName):
+    idiom = IDIOM_DEFAULT
+    tmp = idiomName.lower()
+    for i, n in IDIOM_NAMES.items():
+        if tmp == n.lower():
+            idiom = i
+            break
+    return idiom
+
 class SearchForm(forms.Form):
     term = forms.CharField(max_length=100)
-    puter = forms.BooleanField(required=False)
-    
+    idiom = forms.ChoiceField(choices=IDIOM_NAMES.items())
+                  
 def search(data):
-    if "puter" in data and data["puter"] in (True, "true"):
+    if data["idiom"] == IDIOM_PUTER:
         dbPath = "database/Puter.db"
     else:
         dbPath = "database/Vallader.db"
+    
     dbPath = os.path.join(os.path.dirname(__file__), dbPath)
     conn = sqlite3.connect(dbPath)
     cursor = conn.cursor()
 
-    if "term" in data and len(data["term"]) > 0:
+    if len(data["term"]) > 0:
         query = data["term"]
     else:
         return []
@@ -37,18 +54,24 @@ def search(data):
 @login_required
 def tschercha(request):
     result = []
+    idiom = IDIOM_DEFAULT
+    
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
+            print form.cleaned_data
+            idiom = form.cleaned_data["idiom"]
             result = search(form.cleaned_data)
     else:
-        if "term" in request.GET:
-            result = search(request.GET)
-            form = SearchForm(initial={"term":request.GET["term"],
-                                       "puter":"puter" in request.GET and request.GET["puter"] == "true"})
-        else:
-            form = SearchForm()
+        idiom = idiomNameToIdiom(request.GET.get("idiom", ""))
+        d = {"term"  : request.GET.get("term", ""),
+             "idiom" : idiom}
+        result = search(d)
+        form = SearchForm(initial=d)
 
-    return render(request, 'tschercha.html',
-                  {'form': form, 'result':result,})
+    return render(request,
+                  'tschercha.html',
+                  {'form': form,
+                   'result':result,
+                   'idiom' : IDIOM_NAMES[idiom]})
 
