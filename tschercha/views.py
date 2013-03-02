c720b5b7283d1cb22a61eea7785e5a967373cb9b
Uli Franke <cls@nebadje.org>
2013-03-02 03:27:54 +0100

    * New rendering features:   * Additional word information   * Different formatting for result components   * X-reference links for words redirecting to others   * Clip board copy of words for easy post-search word processing

--- tschercha/views.py
+++ tschercha/views.py
@@ -8,6 +8,8 @@ import sqlite3, os
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django import forms
+from django.utils.html import escape
+from django.utils.http import urlquote
 
 IDIOM_VALLADER = '0'
 IDIOM_PUTER    = '1'
@@ -27,11 +29,12 @@ def idiomNameToIdiom(idiomName):
     return idiom
 
 class SearchForm(forms.Form):
-    term = forms.CharField(max_length=100)
+    term = forms.CharField(max_length=100, required=False)
     idiom = forms.ChoiceField(choices=IDIOM_NAMES.items())
                   
 def search(data):
-    if data["idiom"] == IDIOM_PUTER:
+    idiom = data["idiom"]
+    if idiom == IDIOM_PUTER:
         dbPath = "database/Puter.db"
     else:
         dbPath = "database/Vallader.db"
@@ -46,10 +49,46 @@ def search(data):
         return []
     
     limit = 200
-    sql = "SELECT m, n FROM dicziunari WHERE m LIKE '%%%s%%' OR n LIKE '%%%s%%' LIMIT 0, %i" % (query, query, limit)
-    cursor.execute(sql)
-    res = cursor.fetchall()
-
+                           # de, beugungen, geschl, bereich 
+                                            # rum, geschl, bereich.
+    cursor.execute("SELECT m, tt, ww, ii,   n, ll, rr FROM dicziunari WHERE m LIKE ? OR n LIKE ? LIMIT 0, ?",
+                   ("%%%s%%" % query, "%%%s%%" % query, limit))
+    rows = cursor.fetchall()
+    res = []
+    for row in rows:
+        keys = [(u'wort',       u'',  u'' ),
+                (u'beugung',    u'',  u'' ),
+                (u'geschlecht', u'{', u'}'),
+                (u'bereich',    u'[', u']'),
+                (u'pled',       u'',  u'' ),
+                (u'gener',      u'{', u'}'),
+                (u'chomp',      u'[', u']'),]
+        def fmt(out, key, inp):
+            inp = inp.strip()
+            if inp is not None and len(inp) > 0:
+                txt = u'<span class="%s">%s%s%s</span>' % (key[0], key[1], escape(inp), key[2])
+                if key[0] in [u'wort', u'pled']:
+                    # TODO regex "cf. auch: vergleichen" (low prio - only 5 words)
+                    p = inp.find(u'cf. ')
+                    if p != -1:
+                        newTerm = inp[p+3:].strip()
+                        # We should generate the relative path by our app url part
+                        # and not hard-code it here
+                        txt = '<a class="xref" href="/tschercha/?idiom=%s&term=%s">%s</a>' % \
+                               (IDIOM_NAMES[idiom], urlquote(newTerm), txt)
+                    else:
+                        cbTxt = inp.replace("\"", "")
+                        txt = '<a class="clipb" href=\'javascript:clipb("%s")\'>%s</a>' % (cbTxt, txt)
+                out.append(txt)
+                    
+        de = []
+        rum = []
+        for i in range(4):
+            fmt(de, keys[i], row[i])
+        for i in range(4, len(row)):
+            fmt(rum, keys[i], row[i])
+        res.append((" ".join(de), " ".join(rum)))
+        
     return res
     
 @login_required
